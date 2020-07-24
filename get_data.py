3b5c688b0772dedd23466bd4a7eb796773876144
yellopple <yellopple@yeah.net>
2020-07-24 16:09:15 +0800

    update data files

--- get_data.py
+++ get_data.py
@@ -10,6 +10,10 @@ Created on Wed Jul 22 16:03:01 2020
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import os
+import time
+
+file='StockData/'
 
 def Get_SP500():
     '''
@@ -21,7 +25,6 @@ def Get_SP500():
     tables = pd.read_html(link)
     SP500 = tables[0]['Symbol'].to_list()
     return SP500
-SP500 = Get_SP500()
 
 def Get_ChinaConcept():
     '''China Concept in US
@@ -31,9 +34,7 @@ def Get_ChinaConcept():
     tables = pd.read_html(link)
     CC = tables[0][1].to_list()[1:] + tables[1][1].to_list()[1:]
     return CC
-ChinaConcept = Get_ChinaConcept()
 
-symbol = 'MMM'
 def Get_US_Equity(symbol, kind = 'hist'):
     '''
     get US stock data from yfinance
@@ -43,16 +44,50 @@ def Get_US_Equity(symbol, kind = 'hist'):
     # ^ returns a named tuple of Ticker objects
     if kind == 'hist':
         # access each ticker using (example)
-        return ticker.history('max')
-us1 = Get_US_Equity(symbol)
+        return ticker.history('max').reset_index()
 
 def Get_CN_Equity(symbol):
     '''
+    NOTE: pending update
     get CN stock data from TuShare
     CN market
     '''
     return
 
+def Get_existing():
+    '''Get existing file names and return csv names
+    '''
+    names = os.listdir(file)
+    csv_names = [x[:-4] for x in names if x[-3:] == 'csv']
+    return csv_names
+    
 
+def Saving(names):
+    got = Get_existing()
+    name_update = list(set(names)-set(got))
+    n = len(name_update)
+    print('total names require update: {}.'.format(n))
+    for i, name in enumerate(name_update):
+        # status check
+        if i % 10 == 0:
+            print(f'{i}/{n}')
+        # name download
+        if name not in got:
+            time.sleep(0.1)
+            df = Get_US_Equity(symbol=name, kind = 'hist')
+            if len(df) > 0: # may get fail
+                df.to_csv(file+name+'.csv', index=False)
 
+if __name__ == '__main__':
+    # sp500 names
+    SP500 = Get_SP500()
+    # china concept stock names
+    ChinaConcept = Get_ChinaConcept()
+#    # sample get price
+#    symbol = 'MMM'
+#    us1 = Get_US_Equity(symbol)
+    
+    # batch get
+    names = ChinaConcept
+    Saving(names)
 
